cc890cd4592b8d8a386dd099bb44f1b00aa2bd29
Josh <joshriches@gmail.com>
2021-05-26 18:31:47 -0600

    styles

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -12,21 +12,21 @@ export default class extends Component {
     render() {
         return (
             <Fragment>
-                <Navbar color="light" light expand="md">
+                <Navbar color="light" light expand="md" id="navigation">
                     <NavbarBrand><Link to="/"><img
                         src="https://fontmeme.com/permalink/180608/a61485dcad61e605f496571c49054d2f.png"
                         alt="Josh Riches"/></Link></NavbarBrand>
                     <Nav className="ml-auto" navbar>
-                        <NavItem><NavLink tag={Link} to="/contact">Contact</NavLink></NavItem>
-                        <NavItem><NavLink tag={Link} to="/projects">Projects</NavLink></NavItem>
+                        <NavItem ><NavLink id="nav-i" tag={Link} to="/contact">Contact</NavLink></NavItem>
+                        <NavItem ><NavLink id="nav-i" tag={Link} to="/projects">Projects</NavLink></NavItem>
                         <NavItem>
                             <NavLink target="_blank" href="https://github.com/Yosh23">
-                                <img src={require("../resources/pictures/github")} alt="github"/>
+                                <img height="60em" src={require("../resources/pictures/github")} alt="github"/>
                             </NavLink>
                         </NavItem>
                         <NavItem>
                             <NavLink target="_blank" href="https://www.linkedin.com/in/josh-riches/">
-                                <img height="32em" src={require("../resources/pictures/linkedin.png")} alt="linkedin"/>
+                                <img height="60em" src={require("../resources/pictures/linkedin.png")} alt="linkedin"/>
                             </NavLink>
                         </NavItem>
                     </Nav>
